f458c23b4f2d7da3c47dd69215712319bc7412e8
Zenildo Pinto <zenildopinto@hotmail.com>
2015-02-26 02:32:05 +0000

    linha de erro

--- src/Principal.java
+++ src/Principal.java
@@ -12,5 +12,6 @@ public class Principal {
     
     public static void main(String[] args) {
         System.out.println("Hello Linux");
+        System.err.println(" Zenild pintodsfadsf");
     }
 }
